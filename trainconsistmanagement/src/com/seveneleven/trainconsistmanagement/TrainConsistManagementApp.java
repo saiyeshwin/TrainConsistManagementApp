b3d8cdf9670cc42e2789335c92c0ae8c24057c6e
saiyeshwin <saiyeshwin@gmail.com>
2026-03-10 15:51:50 +0530

    Coded UC7 to sort bogies using comparator

--- trainconsistmanagement/src/com/seveneleven/trainconsistmanagement/TrainConsistManagementApp.java
+++ trainconsistmanagement/src/com/seveneleven/trainconsistmanagement/TrainConsistManagementApp.java
@@ -1,22 +1,39 @@
-// Use Case-06: Map Bogie to Capacity
-// Stores bogie types as keys
-// Maps each bogie to its capacity
-// Displays bogie-capacity details
+// Use Case-07:  Sort Bogies by Capacity (Comparator)
+// We sort passenger bogies by using comparator
 // @author Developer
-// @version 6.0
+// @version 7.0
 
 package com.seveneleven.trainconsistmanagement;
 import java.util.*;
 public class TrainConsistManagementApp {
+	static class Bogie{
+		String name;
+		int capacity;
+		public Bogie(String name,int capacity) {
+			this.name=name;
+			this.capacity=capacity;
+		}
+		@Override
+		public String toString() {
+			return name + "->" + capacity;
+		}
+	}
 	public static void main(String[] args) {
-		Map<String,Integer> capacityMap=new HashMap<>();
-        capacityMap.put("First Class",24);
-        capacityMap.put("Cargo",120);
-        capacityMap.put("Sleeper",72);
-        capacityMap.put("AC Chair",56);
-        System.out.println("Bogie capacity detials:\n");
-        for(Map.Entry<String,Integer> entry:capacityMap.entrySet()) {
-        	System.out.println(entry);
+		List<Bogie> bogies=new ArrayList<>();
+		bogies.add(new Bogie("Sleeper",72));
+		bogies.add(new Bogie("First Class",24));
+		bogies.add(new Bogie("Cargo",120));
+		bogies.add(new Bogie("AC Chair",56));
+		System.out.println("Before Sorting\n");
+		for (Bogie b : bogies) {
+            System.out.println(b);
         }
+		System.out.println();
+		bogies.sort((b1,b2)->b1.capacity-b2.capacity);
+		System.out.println("After Sorting \n");
+		for (Bogie b : bogies) {
+            System.out.println(b);
+        }
+		System.out.println();
 	}
 }
