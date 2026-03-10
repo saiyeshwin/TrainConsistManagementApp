780e7510d32ed3bc75e7b39cda54dea2cac0b576
saiyeshwin <saiyeshwin@gmail.com>
2026-03-10 15:06:11 +0530

    Coded UC6 to map bogies to capacity

--- trainconsistmanagement/src/com/seveneleven/trainconsistmanagement/TrainConsistManagementApp.java
+++ trainconsistmanagement/src/com/seveneleven/trainconsistmanagement/TrainConsistManagementApp.java
@@ -1,23 +1,22 @@
-// Use Case-05:  Preserve Insertion Order of Bogies
-// A train consist must follow a physical sequence but also must be unique
-// Hence we use a LinkedHashSet
+// Use Case-06: Map Bogie to Capacity
+// Stores bogie types as keys
+// Maps each bogie to its capacity
+// Displays bogie-capacity details
 // @author Developer
-// @version 5.0
+// @version 6.0
 
 package com.seveneleven.trainconsistmanagement;
 import java.util.*;
 public class TrainConsistManagementApp {
 	public static void main(String[] args) {
-		Set<String> trainConsist=new LinkedHashSet<>();
-        trainConsist.add("Engine");
-        trainConsist.add("Sleeper");
-        trainConsist.add("AC");
-        trainConsist.add("Cargo");
-        trainConsist.add("Guard");
-        trainConsist.add("Guard");
-        System.out.println("Final train consist:");
-        System.out.println(trainConsist);
-        System.out.println("\nLinkedHashSet ensures insertion order and ensures uniqueness");
-       
+		Map<String,Integer> capacityMap=new HashMap<>();
+        capacityMap.put("First Class",24);
+        capacityMap.put("Cargo",120);
+        capacityMap.put("Sleeper",72);
+        capacityMap.put("AC Chair",56);
+        System.out.println("Bogie capacity detials:\n");
+        for(Map.Entry<String,Integer> entry:capacityMap.entrySet()) {
+        	System.out.println(entry);
+        }
 	}
 }
