76e8cfe4b34d0f9e280642114ea1133e7e73af26
saiyeshwin <saiyeshwin@gmail.com>
2026-03-10 14:53:46 +0530

    Coded UC5 using LinkedHashSet to maintain uniqueness and also insertion order of bogies

--- trainconsistmanagement/src/com/seveneleven/trainconsistmanagement/TrainConsistManagementApp.java
+++ trainconsistmanagement/src/com/seveneleven/trainconsistmanagement/TrainConsistManagementApp.java
@@ -1,31 +1,23 @@
-// Use Case-04: Maintain Ordered Bogie IDs 
-// HashSet does not preserve order.
-// A train consists must follow a physical sequence, so a LinkedList is used to maintain order
+// Use Case-05:  Preserve Insertion Order of Bogies
+// A train consist must follow a physical sequence but also must be unique
+// Hence we use a LinkedHashSet
 // @author Developer
-// @version 4.0
+// @version 5.0
 
 package com.seveneleven.trainconsistmanagement;
 import java.util.*;
 public class TrainConsistManagementApp {
 	public static void main(String[] args) {
-		List<String> trainConsist=new LinkedList<>();
+		Set<String> trainConsist=new LinkedHashSet<>();
         trainConsist.add("Engine");
         trainConsist.add("Sleeper");
         trainConsist.add("AC");
         trainConsist.add("Cargo");
         trainConsist.add("Guard");
-        System.out.println("Initial train consist:");
+        trainConsist.add("Guard");
+        System.out.println("Final train consist:");
         System.out.println(trainConsist);
-        System.out.println();
+        System.out.println("\nLinkedHashSet ensures insertion order and ensures uniqueness");
        
-        trainConsist.add(2,"Pantry Car");
-        System.out.println("After inserting Pantry Car at index 2:");
-        System.out.println(trainConsist);
-        System.out.println();
-        
-        trainConsist.removeFirst();
-        trainConsist.removeLast();
-        System.out.println("After removing first and last bogie:");
-        System.out.println(trainConsist);
 	}
 }
