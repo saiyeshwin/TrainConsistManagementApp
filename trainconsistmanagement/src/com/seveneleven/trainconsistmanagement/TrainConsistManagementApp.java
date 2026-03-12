5014aaf3b13b570595eb5d0bae32e750d12cd869
saiyeshwin <saiyeshwin@gmail.com>
2026-03-12 10:58:06 +0530

    Coded UC15 to safely assign cargos using try-catch-finally

--- trainconsistmanagement/src/com/seveneleven/trainconsistmanagement/TrainConsistManagementApp.java
+++ trainconsistmanagement/src/com/seveneleven/trainconsistmanagement/TrainConsistManagementApp.java
@@ -1,41 +1,45 @@
-// Use Case-14:Handle Invalid Bogie Capacity
+// Use Case-15:Safe Cargo Assignment Using try-catch-finally
 // Prevents invalid passenger bogies from being created
 // If a bogie has zero or negative capacity, a custom exception is thrown.
 // @author Developer
-// @version 14.0
+// @version 15.0
 package com.seveneleven.trainconsistmanagement;
-import java.util.*;
-class InvalidCapacityException extends Exception {
-	public InvalidCapacityException(String message) {
-		super(message);
-	}
-}
 public class TrainConsistManagementApp {
-	static class Bogie {
-		String name;
-		int capacity;
-		Bogie(String name, int capacity) throws InvalidCapacityException {
-			if (capacity<= 0) {
-				throw new InvalidCapacityException("Capacity must be greater than zero");
-			}
-			this.name = name;
-			this.capacity = capacity;
+	static class CargoSafetyException extends RuntimeException {
+		public CargoSafetyException(String message) {
+			super(message);
 		}
-		@Override
-		public String toString() {
-			return name+"->"+capacity;
+	}
+	static class GoodsBogie {
+		String shape;
+		String cargo;
+		GoodsBogie(String shape) {
+			this.shape = shape;
+		}
+		void assignCargo(String cargo) {
+			try {
+				if (shape.equals("Rectangular") && cargo.equals("Petroleum")) {
+					throw new CargoSafetyException("Unsafe cargo assignment!");
+				}
+				this.cargo = cargo;
+				System.out.println("Cargo assigned successfully-" + cargo);
+				System.out.println("Cargo validation completed for "+shape+" bogie");
+			} 
+			catch (CargoSafetyException e) {
+				System.out.println("Error:"+ e.getMessage());
+				System.out.println("Cargo validation completed for " + shape + " bogie");
+			} 
+			finally {
+				System.out.println();
+			}
 		}
 	}
 
 	public static void main(String[] args) {
-		try {
-			Bogie sleeper = new Bogie("Sleeper",72);
-			System.out.println("Created Bogie:"+sleeper);
-			Bogie faulty = new Bogie("Cargo",-4);
-			System.out.println("Created Bogie:"+faulty);
-		} 
-		catch (InvalidCapacityException e) {
-			System.out.println("Error:" + e.getMessage());
-		}
+		GoodsBogie cylindrical = new GoodsBogie("Cylindrical");
+		cylindrical.assignCargo("Petroleum");
+
+		GoodsBogie rectangular = new GoodsBogie("Rectangular");
+		rectangular.assignCargo("Petroleum");
 	}
 }
